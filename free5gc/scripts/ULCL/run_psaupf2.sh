489d7e528576033a0b7fded8d53aed9516902824
IncludeArthur <arturo.bellin@gmail.com>
2023-02-14 17:15:29 +0100

    fixed upf forwarding

--- free5gc/scripts/ULCL/run_psaupf2.sh
+++ free5gc/scripts/ULCL/run_psaupf2.sh
@@ -7,4 +7,10 @@ LOG_NAME="free5gc.log"
 
 /free5gc/bin/upf -c /free5gc/config/ULCL/psaupfcfg2.yaml -l ${LOG_PATH}psaupf2.log -lc ${LOG_PATH}${LOG_NAME} &
 
-sleep 0.1
+sleep 0.5
+
+sysctl -w net.ipv4.ip_forward=1
+iptables -I INPUT --source 10.60.0.0/16 -j ACCEPT
+iptables -t nat -I POSTROUTING --out-interface eth0 -j MASQUERADE
+iptables -I FORWARD --in-interface eth0 --out-interface upfgtp -j ACCEPT
+iptables -I FORWARD --in-interface oupfgtp --out-interface eth0 -j ACCEPT
